c63f78acfdb38396b992cfabf722c74f68c2b0b1
Shivani-Khare <khareshivani.j@gmail.com>
2022-03-29 00:22:22 +0530

    logdirectory_code

---
+++ 3_Implementation/source_code/logdirectory.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+#include<windows.h>
+#include<stdlib.h>
+#include"logdirectory.h"
+
+void logdirectory()
+{
+	FILE *f;
+	int i;
+	if((f=fopen("ott.txt","rb"))==NULL)
+		exit(0);
+	system("cls");
+	printf("Phone Number\t\tUser Name\t\tBilling Amount\n");
+	for(i=0;i<79;i++)
+		printf("-");
+	while(fread(&s,sizeof(s),1,f)==1)
+	{
+		printf("\n%s\t\t%s\t\tRs. %.2f /-",s.phonenumber,s.name,s.billingamount);
+	}
+	printf("\n");
+	for(i=0;i<79;i++)
+		printf("-");
+
+fclose(f);
+printf("\n\nPress Any Key To Go Back");
+getch();
+system("cls");
+}
